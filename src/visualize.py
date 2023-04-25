49ddfeae39e00012f01e68197c0ad7fcf3672411
Abizer Mamnoon <amamnoon25@cmc.edu>
2023-04-25 01:41:48 -0700

    modify readme

--- src/visualize.py
+++ src/visualize.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
 
 # command line args
 import argparse
@@ -24,5 +26,39 @@ if args.percent:
 
 # print the count values
 items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
-for k,v in items:
-    print(k,':',v)
+#for k,v in items[0:11]:
+#    print(k,':',v)
+# Get the top 11 items sorted by y-value in descending order
+sorted_data = sorted(items[0:10], key=lambda item: item[1], reverse=False)
+#sorted_data = sorted(sorted_data, key=lambda item: item[1])
+#print("sorted_data=", sorted_data)
+
+## Extract the x and y values
+#x = [item[0] for item in sorted_data]
+#y = [item[1] for item in sorted_data]
+#print("y=", y)
+x = range(len(sorted_data)) 
+y = [item[1] for item in sorted_data]
+label = [item[0] for item in sorted_data] 
+print("x,y=", x,y)
+plt.bar(x, y)
+
+print("(x,y)=", (x,y))
+# Create the bar plot
+
+plt.xticks(x,label)
+
+# Set the x-axis label and y-axis label
+plt.xlabel("Country")
+plt.ylabel("Volume of Tweets")
+
+# Set the font family for the plot title
+plt.rcParams['font.family'] = 'NanumGothic'
+
+## Set the title of the plot
+#plt.title("#Coronavirus Occurrences in each Language on Twitter")
+
+plt.savefig("covid_country_ko")
+
+# Display the plot
+plt.show()
